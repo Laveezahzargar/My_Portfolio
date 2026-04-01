890a01656029c143b0452a00cde092e57b01adc6
Munizazargar <munizahzargar.iimun@gmail.com>
2026-04-01 13:58:20 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"]
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio - Laveezah Zargar",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth ">
+      <body className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden bg-white text-black dark:bg-[#11001F] dark:text-white`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}", // keep this even if empty
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      boxShadow: {
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff'
+      },
+      gridTemplateColumns: {
+        'autoFit': 'repeat(auto-fit, minmax(200px, 1fr))'
+      }
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+}
