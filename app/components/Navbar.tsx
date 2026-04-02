e658d979c0d89e67cf5a4b227d7da976d65987f0
Laveezahzargar <laveezahzargar@gmail.com>
2026-04-02 13:29:19 +0530

    mobileMenu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -64,7 +64,7 @@ const Navbar = ({ isDarkMode, setIsDarkMode }: ThemeProps) => {
         </div>
 
         {/* mobile menu */}
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed right-0 transform translate-x-full
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed right-0
          top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 dark:bg-[#2a004a] transition duration-500'>
 
           <div className='absolute right-6 top-6' onClick={closeMenu}>
